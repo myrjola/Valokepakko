2d9c4128898c8504813e6ea42eb2d634cf7e56a1
Martin Yrjölä <martin.yrjola@gmail.com>
2015-11-25 14:57:27 +0200

    Switch to using palette in the converter

--- kepakkoconverter.py
+++ kepakkoconverter.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import PIL
+import sys
 from PIL import Image
 
 LED_COUNT = 60
@@ -13,6 +14,23 @@ def resize_image(path):
     return img.resize((int(old_width*ratio), int(old_height*ratio)),
                       PIL.Image.ANTIALIAS)
 
+
+def reduce_palette(img):
+    """
+    Reduces palette to 256 colors.
+    """
+    return img.convert('P', dither=Image.FLOYDSTEINBERG,
+                       palette=Image.ADAPTIVE, colors=256)
+
+def get_palette_grouped(img):
+    """
+    Returns the img palette as list of rgb values.
+    """
+    palette = img.getpalette()
+    return zip(*(iter(palette),) * 3)
+
+
+
 def print_matrix(img):
     """
     Arduino matrices can be defined like this:
@@ -22,11 +40,22 @@ def print_matrix(img):
         {2, 3, 4}
     };
     """
+    palette = get_palette_grouped(img)
+    rgb = ["{{{0}, {1}, {2}}}".format(*color) for color in palette]
+    pal_decl = "byte palette[{0}][3] = {{{1}}};".format(len(rgb),
+                                                        ",\n".join(rgb))
     pixels = img.getdata()
-    rgb = ["{{{0}, {1}, {2}}}".format(*pixel) for pixel in pixels]
-    declaration = "byte pixels[{0}][3] = {{{1}}};".format(len(pixels),
-                                                          ",\n".join(rgb))
-    print(declaration)
+    pixels_as_str = ",\n".join(str(pixel) for pixel in pixels)
+    pixels_decl = "byte pixels[{0}] = {{{1}}};".format(len(pixels),
+                                                       pixels_as_str)
+    print("\n".join([pal_decl, pixels_decl]))
 
 if __name__ == "__main__":
-    print_matrix(resize_image("img/grin.png"))
+    image_path = "img/grin.png"
+    if len(sys.argv) > 1:
+        image_path = sys.argv[1]
+
+    img = resize_image(image_path)
+    img_converted = reduce_palette(img)
+
+    print_matrix(img_converted)
